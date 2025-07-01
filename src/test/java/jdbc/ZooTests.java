26539fa611b1ab08d512a5dfdca7dfdc96f3b4b7
lachatonfou <maniakitten@mail.ru>
2025-07-01 21:10:19 +1000

    создала CRUD для jdbc

--- src/test/java/jdbc/ZooTests.java
+++ src/test/java/jdbc/ZooTests.java
@@ -1,4 +1,93 @@
 package jdbc;
 
+import jdbc.models.Animal;
+import jdbc.utils.DatabaseUtils;
+import jdbc.utils.CRUDUtils;
+import jdbc.models.Places;
+import jdbc.utils.DatabaseConnection;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ZooTests {
+    @BeforeAll
+    static void init() {
+        DatabaseUtils.createData();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        DatabaseConnection.closeConnection();
+    }
+
+    @Test
+    void createAnimal() {
+        Animal newAnimal = new Animal(11, "Муся", 3, 2, 2, 2);
+        int initialCount = CRUDUtils.getAnimalCountRow();
+        CRUDUtils.insertAnimalData(newAnimal);
+        int newCount = CRUDUtils.getAnimalCountRow();
+        assertEquals(initialCount + 1, newCount);
+
+        List<Animal> animals = CRUDUtils.getAnimalData("SELECT * FROM public.animal WHERE Id =" + newAnimal.getId());
+        Animal addedAnimal = animals.getLast();
+        assertEquals(newAnimal.getId(), addedAnimal.getId());
+        assertEquals(newAnimal.getName(), addedAnimal.getName());
+        assertEquals(newAnimal.getAge(), addedAnimal.getAge());
+        assertEquals(newAnimal.getType(), addedAnimal.getType());
+        assertEquals(newAnimal.getSex(), addedAnimal.getSex());
+        assertEquals(newAnimal.getPlace(), addedAnimal.getPlace());
+    }
+
+    @Test
+    void readAnimal() {
+        List<Animal> animals = CRUDUtils.getAnimalData("SELECT * FROM public.animal");
+        Animal firstAnimal = animals.getFirst();
+        assertFalse(animals.isEmpty());
+        assertEquals(firstAnimal.getId(), 1);
+        assertEquals(firstAnimal.getName(), "Бусинка");
+        assertEquals(firstAnimal.getAge(), 2);
+        assertEquals(firstAnimal.getType(), 1);
+        assertEquals(firstAnimal.getSex(), 1);
+        assertEquals(firstAnimal.getPlace(), 1);
+    }
+
+    @Test
+    void updateAnimal() {
+        List<Animal> animals = CRUDUtils.getAnimalData("SELECT * FROM public.animal");
+        Animal firstAnimal = animals.getFirst();
+        firstAnimal.setName("Пельмешка");
+        firstAnimal.setPlace(3);
+
+        assertEquals(firstAnimal.getName(), "Пельмешка");
+        assertEquals(firstAnimal.getPlace(), 3);
+    }
+
+    @Test
+    void deleteAnimal() {
+        int initialCount = CRUDUtils.getAnimalCountRow();
+        Animal newAnimal = new Animal(11, "Муся", 3, 2, 2, 2);
+        CRUDUtils.insertAnimalData(newAnimal);
+        int newCount = CRUDUtils.getAnimalCountRow();
+        assertEquals(initialCount + 1, newCount);
+
+        CRUDUtils.deleteAnimalById(11);
+        int finalCount = CRUDUtils.getAnimalCountRow();
+        assertEquals(initialCount, finalCount);
+
+        List<Animal> animals = CRUDUtils.getAnimalData("SELECT * FROM public.animal WHERE Id =" + newAnimal.getId());
+        assertTrue(animals.isEmpty());
+    }
 }
--- src/test/java/jdbc/utils/CRUDUtils.java
+++ src/test/java/jdbc/utils/CRUDUtils.java
@@ -10,12 +10,8 @@ import java.util.List;
 
 public class CRUDUtils {
     private static final String insertAnimal = "INSERT INTO public.animal (id, \"name\", age, \"type\", sex, place) VALUES(?, ?, ?, ?, ?, ?);\n";
-    private static final String insertWorkman = "INSERT INTO public.workman (id, \"name\", age, \"position\") VALUES(1, null, 23, 1);\n";
-    private static final String insertPlaces = "INSERT INTO public.places (id, \"row\", place_num, \"name\") VALUES(?, ?, ?, ?);\n";
-    private static final String insertZoos = "INSERT INTO public.zoo (id, \"name\") VALUES(?, ?, );\n";
-
+    private static final String deleteAnimal = "DELETE FROM public.animal WHERE id = ?";
     private static final String getCountAnimalRow = "SELECT count(*) from animal";
-    private static final String getCountPlacesRow = "SELECT count(*) from places";
 
     public static List<Animal> getAnimalData(String query) {
         List<Animal> animals = new ArrayList<>();
@@ -53,51 +49,12 @@ public class CRUDUtils {
         preparedStatement.executeUpdate();
     }
 
-    public static boolean insertWorkmanData() {
-        try {
-            Connection connection = DatabaseConnection.createConnection();
-            Statement statement = connection.createStatement();
-
-            statement.executeUpdate(insertWorkman);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    public static boolean insertPlacesData(Places place){
-        try {
-            Connection connection = DatabaseConnection.createConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(insertPlaces);
-            preparedStatement.setInt(1, place.getId());
-            preparedStatement.setInt(2, place.getRow());
-            preparedStatement.setInt(3, place.getPlace_num());
-            preparedStatement.setString(4, place.getName());
-            preparedStatement.executeUpdate();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        return true;
-    }
-
-    public static int getCountRowByTable(String tableName) throws SQLException {
+    @SneakyThrows
+    public static void deleteAnimalById(int id) {
         Connection connection = DatabaseConnection.createConnection();
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT count (*) from " + tableName);
-        resultSet.next();
-        int count = resultSet.getInt("count(*)");
-        System.out.printf("В таблице public.%s ровно %s записей%n", tableName, count);
-        return count;
-    }
-
-    public static int getZooCountRow() throws SQLException {
-        Connection connection = DatabaseConnection.createConnection();
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT count (*) from zoo");
-        resultSet.next();
-        int count = resultSet.getInt("count(*)");
-        System.out.println(resultSet.getInt("count(*)"));
-        return count;
+        PreparedStatement preparedStatement = connection.prepareStatement(deleteAnimal);
+        preparedStatement.setInt(1, id);
+        preparedStatement.executeUpdate();
     }
 
     @SneakyThrows
@@ -107,44 +64,5 @@ public class CRUDUtils {
         ResultSet resultSet = statement.executeQuery(getCountAnimalRow);
         resultSet.next();
         return resultSet.getInt("count(*)");
-    }
-
-    public static int getRowCountByTable(String tableName) throws SQLException {
-        Connection connection = DatabaseConnection.createConnection();
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT count (*) from " + tableName);
-        resultSet.next();
-        int count = resultSet.getInt("count(*)");
-        System.out.printf("Table public.%s has exact %s rows%n", tableName, count);
-        return count;
-    }
-
-    public static int getPlacesCountRow() throws SQLException {
-        Connection connection = DatabaseConnection.createConnection();
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT count (*) from places");
-        resultSet.next();
-        int count = resultSet.getInt("count(*)");
-        System.out.println(resultSet.getInt("count(*)"));
-        return count;
-    }
-
-    public static List<String> getZooNameData() {
-        String name;
-        List<String> zoo = new ArrayList<>();
-        try {
-            Connection connection = DatabaseConnection.createConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT id,  \"name\" from public.zoo");
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()) {
-                name = resultSet.getString("name");
-                System.out.println(name);
-                zoo.add(name);
-            }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        return zoo;
     }
 }
